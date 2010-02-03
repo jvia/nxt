27c532904bdcfe72b2ba17e75648fc1f4c75c64e
Jeremiah Via <jeremiah.via@gmail.com>
2010-02-03 20:32:04 +0000

    distance commenting done

--- trunk/robot/src/distance/MoveForward.java
+++ trunk/robot/src/distance/MoveForward.java
@@ -1,41 +1,42 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package distance;
 
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.Pilot;
 
 /**
- * 
- * @author Jeremiah Via
+ * Describes the behavior of moving forward.
+ *
+ * Exercise 03, Part 01
+ * 4 February 2010
+ * @author Jeremiah Via, Michal Staniazek
  */
 public class MoveForward extends Move {
 
     /**
-     * 
-     * @param distance
-     * @param pilot
-     * @param ultrasonic
+     * Constructs behavior
+     * @param distance distance to maintain between object and robot
+     * @param pilot pilot used to move the robot
+     * @param us ultrasonic sensor to measure distance
      */
     public MoveForward(int distance, Pilot pilot, UltrasonicSensor ultrasonic) {
         super(distance, pilot, ultrasonic);
     }
 
     /**
-     *
-     * @return
+     * Decides if behavior should take control
+     * @return true if distance + ERROR_THRESHOLD is greater than the
+     * distance measured
      */
     @Override
     public boolean takeControl() {
-        return ultrasonic.getDistance() + ERROR_THRESHOLD > distance ? true : false;
+        return (ultrasonic.getDistance() + ERROR_THRESHOLD > distance)
+               ? true
+               : false;
     }
 
-    /**
-     *
-     */
+    /** Moves robot forward while take control condition is true. */
     @Override
-    public void action() { pilot.forward(); }
-
+    public void action() {
+        pilot.forward();
+    }
 }
--- trunk/robot/src/distance/SitStill.java
+++ trunk/robot/src/distance/SitStill.java
@@ -2,41 +2,43 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package distance;
+
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.Pilot;
+
 /**
  * 
- * @author Jeremiah Via
+ * 
+ * Exercise 03, Part 01
+ * 4 February 2010
+ * @author Jeremiah Via, Michal Staniazek
  */
-public class SitStill extends Move
-{
+public class SitStill extends Move {
 
     /**
-     *
-     * @return
-     */
-    @Override
-    public boolean takeControl() {
-        return ultrasonic.getDistance() < distance + ERROR_THRESHOLD &&
-                ultrasonic.getDistance() > distance - ERROR_THRESHOLD;
-
-    }
-
-    /**
-     *
-     * @param distance
-     * @param pilot
-     * @param ultrasonic
+     * Constructs behavior
+     * @param distance distance to maintain between object and robot
+     * @param pilot pilot used to move the robot
+     * @param us ultrasonic sensor to measure distance
      */
     public SitStill(int distance, Pilot pilot, UltrasonicSensor ultrasonic) {
         super(distance, pilot, ultrasonic);
     }
 
     /**
-     * 
+     * Decides if behavior should take contorl
+     * @return true if distance - ERROR_THRESHOLD is less than measured
+     * distance and less than distance + ERROR_THRESHOLD, false otherwise
      */
+    @Override
+    public boolean takeControl() {
+        return ultrasonic.getDistance() < distance + ERROR_THRESHOLD && ultrasonic.
+                getDistance() > distance - ERROR_THRESHOLD;
+
+    }
+
+    /** Stops the robot. */
     @Override
     public void action() {
         pilot.stop();
