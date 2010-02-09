ba3c4d52f8c007c8a9a436e0d7836ee0fb497487
Michal Staniaszek <m.staniaszek@gmail.com>
2010-02-09 16:45:59 +0000

    junctions stuff

---
+++ trunk/robot/src/line/lightSensor/JunctionReached.java
@@ -0,0 +1,38 @@
+package line.lightSensor;
+
+import lejos.nxt.LightSensor;
+import lejos.robotics.navigation.Pilot;
+import lejos.robotics.subsumption.Behavior;
+
+/**
+ * 
+ * @author Jeremiah Via
+ */
+public class JunctionReached implements Behavior {
+
+    public static int BLACK = 40;
+    private LightSensor leftSensor;
+    private LightSensor rightSensor;
+    private Pilot pilot;
+
+    public JunctionReached(LightSensor leftSensor, LightSensor rightSensor, Pilot pilot) {
+        this.leftSensor = leftSensor;
+        this.rightSensor = rightSensor;
+        this.pilot = pilot;
+    }
+
+    public boolean takeControl() {
+        return (leftSensor.getLightValue() <= BLACK
+               && rightSensor.getLightValue() <= BLACK);
+    }
+
+    public void action() {
+        pilot.stop();
+
+    }
+
+    public void suppress() {
+        pilot.stop();
+    }
+
+}
